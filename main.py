93e615b91b6bd7fd5198361dd897521bba64b931
Divya Pundir <46810194+pundirdiya898@users.noreply.github.com>
2020-08-22 13:07:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# This entrypoint file to be used in development. Start by reading README.md
+from arithmetic_arranger import arithmetic_arranger
+from unittest import main
+
+print(arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]))
+print("\n\n")
+
+print(arithmetic_arranger(
+    ["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"]))
+print("\n\n")
+
+print(arithmetic_arranger(["44 + 815", "909 - 2",
+                           "45 + 43", "123 + 49", "888 + 40", "653 + 87"]))
+print("\n\n")
+
+print(arithmetic_arranger(["3 / 855", "3801 - 2", "45 + 43", "123 + 49"]))
+print("\n\n")
+
+print(arithmetic_arranger(["24 + 85215", "3801 - 2", "45 + 43", "123 + 49"]))
+print("\n\n")
+
+print(arithmetic_arranger(["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"]))
+print("\n\n")
+
+print(arithmetic_arranger(
+    ["32 - 698", "1 - 3801", "45 + 43", "123 + 49"], True))
+print("\n\n")
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
